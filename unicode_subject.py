d19a7c044b1aceab1bd5ae13f03496cc7c3596fe
Siltaar <siltaar@d12s.fr>
2017-03-23 10:44:03 +0100

    re-inflate l to line

--- unicode_subject.py
+++ unicode_subject.py
@@ -7,5 +7,5 @@ import io, sys
 from email.header import decode_header as d
 from email.header import make_header as m
 
-instream = io.TextIOWrapper(sys.stdin.buffer, errors='ignore')
-print(next(m(d(l)) for l in instream if l.startswith('ubject', 1)))
+in_stream = io.TextIOWrapper(sys.stdin.buffer, errors='ignore')
+print(next(m(d(line)) for line in in_stream if line.startswith('ubject', 1)))
